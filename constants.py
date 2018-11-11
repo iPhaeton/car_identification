de1391ec33958cc77ca5b5f174141c1c9b204948
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2018-11-11 12:13:10 +0300

    Can use grayscale images for feature extractor

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
 img_size=224
 source_dir_train='data/train_dataset/'
 source_dir_dev='data/dev_dataset/'
+source_dir_train_gray='data/train_dataset_gray'
+source_dir_dev_gray='data/dev_dataset_gray'
