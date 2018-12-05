7782b9afdfced1a3237de4673812b6a8470be183
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2018-12-05 19:20:08 +0300

    Define PlotLosses module

---
+++ modules/plot_losses.py
@@ -0,0 +1,45 @@
+import keras
+from matplotlib import pyplot as plt
+from IPython.display import clear_output
+
+class PlotLosses(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.i = 0
+        self.x = []
+        self.losses = []
+        self.acc = []
+        self.val_losses = []
+        self.val_acc = []
+        
+        self.logs = []
+
+    def on_epoch_end(self, epoch, logs={}):
+        f, (ax1, ax2) = plt.subplots(2, sharex=True, sharey=False)
+        self.ax1 = ax1
+        self.ax2 = ax2
+    
+        self.logs.append(logs)
+        self.x.append(self.i)
+        self.losses.append(logs.get('loss'))
+        self.acc.append(logs.get('categorical_accuracy'))
+        self.val_losses.append(logs.get('val_loss'))
+        self.val_acc.append(logs.get('val_categorical_accuracy'))
+        self.i += 1
+        
+        clear_output(wait=True)
+        self.ax1.plot(self.x, self.losses, label="loss")
+        self.ax1.plot(self.x, self.val_losses, label="val loss")
+        self.ax1.legend()
+        self.ax2.plot(self.x, self.acc, label="accuracy")
+        self.ax2.plot(self.x, self.val_acc, label="val accuracy")
+        self.ax2.legend()
+        plt.show()
+        
+        for i in range(self.i):
+            print('Epoch ' + str(i+1))
+            print('-----------------------')
+            print('- Loss:', self.losses[i])
+            print('- Accuracy:', self.acc[i])
+            print('- Validation loss:', self.val_losses[i])
+            print('- Validation accuracy:', self.val_acc[i])
+            print(' ')
