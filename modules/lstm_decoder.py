daab3f76418457d457ba25e7053fb20562978a61
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2018-12-06 11:02:12 +0300

    Can train using LSTMDecoder class

--- modules/lstm_decoder.py
+++ modules/lstm_decoder.py
@@ -5,13 +5,16 @@ import numpy as np
 import tensorflow as tf
 
 class LSTMDecoder:
-    def __init__(self, X, y, weights_path = None):
-        self.X = X
-        self.y = y
-        self.y_onehot = to_categorical(y)
+    def __init__(self, X_train, y_train, X_dev, y_dev, lr=0.00001, weights_path = None):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_dev = X_dev
+        self.y_dev = y_dev
+        self.y_train_hot = to_categorical(self.y_train) if y_train != None else None
+        self.y_dev_hot = to_categorical(self.y_dev)
 
-        input_shape = X.shape[1:]
-        output_shape = self.y_onehot.shape[1]
+        input_shape = self.X_dev.shape[1:]
+        output_shape = self.y_dev_hot.shape[1]
 
         inputs = Input(shape=input_shape)
         X = BatchNormalization()(inputs)
@@ -20,7 +23,7 @@ class LSTMDecoder:
 
         model = Model(inputs=inputs, outputs=outputs)
         model.compile(
-            optimizer=tf.train.AdamOptimizer(0.000001),
+            optimizer=tf.train.AdamOptimizer(lr),
             loss='categorical_crossentropy',
             metrics=['categorical_accuracy'],
         )
@@ -30,6 +33,18 @@ class LSTMDecoder:
 
         self.model = model
 
-    def predict(self):
-        predictions = self.model.predict(self.X, verbose = True)
+    def train(self, epochs, callbacks, batch_size=128):
+        self.model.fit(
+            self.X_train,
+            self.y_train_hot,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=True,
+            callbacks=callbacks,
+            validation_data=(self.X_dev, self.y_dev_hot)
+        )
+
+    def predict(self, mode):
+        X = self.X_dev if mode == 'dev' else self.X_train
+        predictions = self.model.predict(X, verbose = True)
         return np.argmax(predictions, axis=1)
