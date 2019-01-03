94e43e729525801db8bb56018dedaaa2ffc3e640
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2019-01-03 16:15:52 +0300

    Can load base models on app start

--- home/app.py
+++ home/app.py
@@ -3,8 +3,30 @@ from modules.detector import Detector
 from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img, array_to_img, save_img
 from werkzeug.utils import secure_filename
 import os
+from keras.applications.resnet50 import ResNet50, preprocess_input as resnet50_preprocess_input
+from keras.applications.inception_v3 import InceptionV3, preprocess_input as inception_preprocess_input
+from keras.applications.xception import Xception, preprocess_input as xception_preprocess_input
+import sys
+sys.path.append("..")
+from utils.models import get_base_model
+from constants import img_size
 
 app = Flask(__name__)
+
+pretrained_models = [
+    {
+        'constructor': ResNet50,
+        'preprocessor': resnet50_preprocess_input,
+    },
+    {
+        'constructor': InceptionV3,
+        'preprocessor': inception_preprocess_input,
+    },
+    {
+        'constructor': Xception,
+        'preprocessor': xception_preprocess_input,
+    }
+]
 
 detector = None
 base_models = None
@@ -16,12 +38,14 @@ def load_detector():
     return detector
 
 def load_base_models():
-    
+    global base_models
+    if base_models == None:
+        base_models = []
+        for model in pretrained_models:
+            base_models.append(get_base_model(model['constructor'], model['preprocessor'], img_size=img_size, pooling='avg', verbose=False))
+    return base_models
 
-# The request method is POST (this method enables your to send arbitrary data to the endpoint in the request body, including images, JSON, encoded-data, etc.)
-@app.route('/', methods=["POST"])
-def evaluate():
-    input_image = request.files.get('image')
+def detect(input_image):
     detector = load_detector()
 
     filename = secure_filename('recognizing_image.jpg')
@@ -44,8 +68,18 @@ def evaluate():
     )
 
     save_img(output_filepath, car_image)
-
     car_image_array = img_to_array(car_image)
+    return output_filepath
+
+def get_base_features():
+    base_models = load_base_models()
+    print(len(base_models))
+
+@app.route('/', methods=["POST"])
+def evaluate():
+    input_image = request.files.get('image')
+    output_filepath = detect(input_image)
+    get_base_features()
     
     return send_file(output_filepath, mimetype='image/jpg')
 
@@ -53,5 +87,7 @@ def evaluate():
 if __name__ == "__main__":
     print('* Loading detection model...')
     load_detector()
+    print('* Loading base models...')
+    load_base_models()
     print("* Starting web server... please wait until server has fully started")
     app.run(host='0.0.0.0', threaded=False)
